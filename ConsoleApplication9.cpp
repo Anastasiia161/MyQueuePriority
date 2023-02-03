6268368fd8d1d3553408040f45de7f77746e9518
Anastasiia161 <118004157+Anastasiia161@users.noreply.github.com>
2023-02-03 14:00:02 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+#include<string>
+#include"MyQueuePriority.h"
+
+using namespace std;
+
+
+int main()
+{
+	MyQueuePriority<string>print(20);
+	print.push("Klient 1", 5);
+	print.push("Klient 2", 2);
+	print.push("Klient 3", 4);
+	print.push("Klient 4", 3);
+
+	int pr = 0;
+
+	while (!print.isEmpty()) {
+		cout << print.top(pr) << "-" << pr << endl;
+		print.pop();
+	}
+}
+
---
+++ MyQueuePriority.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+// ќчередь с приоритетом
+#include<iostream>
+using namespace std;
+template <typename T>
+class MyQueuePriority
+{
+protected:
+	int maxcounter = 0;
+	int counter = 0;
+	T* queue = nullptr;
+	int* priority = nullptr;
+public:
+	explicit MyQueuePriority(int maxcounter) :maxcounter{ maxcounter } {
+		queue = new T[maxcounter];
+		priority = new int[maxcounter] {};
+	}
+	~MyQueuePriority() {
+		if (queue != nullptr)
+			delete[] queue;
+		if (priority != nullptr)
+			delete[] priority;
+	}
+	void push(T& val, int priority = 0) {
+		if (counter < maxcounter) {
+			queue[counter] = val;
+			this->priority[counter++] = priority;
+
+			int k = counter - 1;
+			while ((k > 0) && (this->priority[k] > this->prioriti[k - 1])) {
+				swap(queue[k], queue[k - 1]);
+				swap(this->priority[k], this->priority[k - 1]);
+				k--;
+			}
+
+		}
+		else
+			cout << "Error! Queue overflow\n";
+	}
+	void push(T&& val, int priority = 0) {
+		if (counter < maxcounter) {
+			queue[counter] = val;
+			this->priority[counter++] = priority;
+
+			int k = counter - 1;
+			while ((k > 0) && (this->priority[k] > this->priority[k - 1])) {
+				swap(queue[k], queue[k - 1]);
+				swap(this->priority[k], this->priority[k - 1]);
+				k--;
+			}
+
+		}
+		else
+			cout << "Error! Queue overflow\n";
+	}
+	T& top(int& priority) const {
+		if (counter) {
+			priority = this->priority[0];
+			return queue[0];
+		}
+	}
+	T& top() const {
+		if (counter)
+			return queue[0];
+	}
+	void pop() {
+		if (counter) {
+			for (int i = 0; i < counter - 1; ++i) {
+				swap(queue[i], queue[i + 1]);
+				swap(priority[i], priority[i + 1]);
+			}
+			--counter;
+		}
+	}
+	bool isEmpty() const {
+		return !counter;
+	}
+};
+
+
